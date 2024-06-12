293b580c13549ce212d871472d83ac1e5f7fdd34
Nolann SALAÜN <149326997+nolann-salaun@users.noreply.github.com>
2024-06-12 21:20:35 +0200

    inserting fruit chosen variables into API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,18 +28,17 @@ max_selections=5)
 
 
 if ingredients_list:
-
     ingredients_string =''
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """', '"""+ name_on_order +"""')"""
-    #st.write(my_insert_stmt)
-    #st.stop()
+   
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
