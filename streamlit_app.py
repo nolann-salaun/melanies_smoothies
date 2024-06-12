58e3f5e0787e9392372ff1c94fea1435ee73e156
Nolann SALAÜN <149326997+nolann-salaun@users.noreply.github.com>
2024-06-12 21:16:30 +0200

    Replacing the API call in the for loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,8 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-    #st.write(ingredients_string)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """', '"""+ name_on_order +"""')"""
@@ -43,10 +44,5 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         
-        
         st.success('Your Smoothie is ordered!', icon="✅")
 
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
